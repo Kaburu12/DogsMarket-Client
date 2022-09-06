74d5249de372a83d597017be5bc602a1355c3b5a
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-09-06 11:04:52 +0300

    create header section

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,30 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 const Home = () => {
   return (
     <div className="home">
-       
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<header></header>
+<section className="section-one">
+  <div className="bar">
+    <h2>Get Your Dream Super Breed Dog With Us</h2>
+    <div className="icons-container">
+      <div className="icon-items">
+        <img src="" alt="" />
+        <p>Free Delivery</p>
+      </div >
+      <div className="icon-items">
+        <img src="" alt="" />
+        <p>Free Medical Check-up</p>
+      </div>
+      <div className="icon-items">
+        <img src="" alt="" />
+        <p>Dog Grooming</p>
+       </div>
+      <div className="icon-items" >
+        <img src="" alt="" />
+        <p>Friendly price</p>
+     </div>
+</div>
+  </div>
         
 
         
