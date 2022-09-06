108ea0f6cd7d109ab80037f4f7240ae124f5d9c1
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-09-06 11:03:13 +0300

    import icons

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   return (
