f121fb7ea22bedef9cc3aa6cecda823d556023fe
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-09-06 11:09:17 +0300

    create the slides

--- src/Home.js
+++ src/Home.js
@@ -30,8 +30,25 @@ const Home = () => {
 </div>
   </div>
         
+  <script type="text/javascript" src="http://wowslider.com/images/effects/rotate.js"></script>  
+<script type="text/javascript" src="http://wowslider.com/images/demo/jquery.js"></script>
+      
+<link rel="stylesheet" type="text/css" href="http://wowslider.com/sliders/demo-57/engine1/style.css" />
 
-        
+<div id="wowslider-container1">
+<div class="ws_images">
+<ul>
+<li id="wowslider"><img style={{width: '45%', height: '50%'}} src="http://www.dogsforsale.co.in/wp-content/uploads/2018/06/labrador-123.jpg" alt="chiwawa" title="Labrador" id="wows1_0"/><button>Labrador</button> </li>
+<li id="wowslider"><img style={{width: '70%', height: '100%'}} src="https://media.istockphoto.com/photos/portrait-of-three-dogs-closeup-picture-id607270080?k=20&m=607270080&s=612x612&w=0&h=UiWL_-Rhz5BNXjq7I1hicgP1OZ8QtvvHZhGPq-hBH1E=" alt="german shepherd" title=" German shepherd" id="wows1_1"/><button>German shepherd</button> </li>
+<li id="wowslider"><img style={{width: '45%', height: '50%'}}  src="http://www.dogsforsale.co.in/wp-content/uploads/2018/06/Golden-Retriever-600x600.jpg" alt="Mountains: slideshow html images" title="Golden Retriever" id="wows1_2"/> <button>Golden retriver</button> </li>
+<li id="wowslider"><img style={{width: '45%', height: '50%'}}  src="http://www.dogsforsale.co.in/wp-content/uploads/2018/06/Rottweiler-600x600.jpg" alt="Yosemite National Park: slideshow html javascript" title="Rottweiler" id="wows1_3"/> <button>Rottweiler</button> </li>
+</ul>
+</div>
+</div>
+        <hr />
+</section> 
+
+      
 
 
     
