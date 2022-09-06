96cc8d3ea261f98d6f17a439ac6e85c70f2883c9
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-09-06 11:00:42 +0300

    create routes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,15 @@ import Services from "./Services";
 function App() {
  
   return (
- 
+    <div>
+    <NavBar />
+    <Routes>
+      <Route path="/doglist" element={<DogList />}/>
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services/>} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </div>
   );
 }
 
