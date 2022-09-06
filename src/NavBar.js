44eceffcdac29f6f3776102a01d87ad5fa50dd0c
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-09-06 10:59:42 +0300

    create a nav bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,33 @@ import { NavLink, Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div id="logo" className="logo nav-common">
-     
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<nav id="nav">
+  <ul className="nav-common">
+    <li>
+      <Link to="/">
+        <p>
+         <span id="logo">Dogs Market-Ke</span> 
+        </p>
+      </Link>
+    </li>
+    <li>
+      <NavLink to="/">Home</NavLink>
+    </li>
+    <li>
+      <NavLink to="/about">About Us </NavLink>
+    </li>
+    <li>
+      <NavLink to="/doglist">Buy & Sell Dogs </NavLink>
+    </li>
+    <li>
+      <NavLink to="/services">Services </NavLink>
+    </li>
+    <li>
+      <NavLink to="#">SignUp/LogIn </NavLink>
+    </li>
+  </ul>
+</nav>
     </div>
   );
 };
