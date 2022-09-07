eb60da4af11417a2ae6d924436fbb9e30f11970d
Michael Kaburu <104422948+Kaburu12@users.noreply.github.com>
2022-09-07 14:06:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-// import React, { useState, useEffect } from "react";
+
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import About from "./About";
 import DogList from "./DogList";
@@ -7,11 +8,35 @@ import Home from "./Home";
 import Services from "./Services";
 
 function App() {
+  const [dogs, setDogs] = useState([]);
+
+    //fectching dogs to be displayed
+    useEffect(() => {
+      fetch("https://server-app123.herokuapp.com/products")
+        .then((res) => res.json())
+        .then((dogs) => setDogs(dogs));
+    }, []);
+  
+    function handleDeleteDog(deleteDog) {
+      const updatedDogs = dogs.filter((dog) => dog.id !== deleteDog.id);
+    setDogs(updatedDogs);
+    }
  
+  
+  //a function meant to update dogs state on adding new dogs
+  function handleAddDog(newDog) {
+    setDogs([ ...dogs, newDog ]);
+    
+
+    
+  }
+  
+
   return (
     <div>
     <NavBar />
     <Routes>
+      <Route path="/doglist" element={<DogList onAddDog={handleAddDog}   dogBreeds={dogs} onDeleteDog={ handleDeleteDog} />}/>
       <Route path="/doglist" element={<DogList />}/>
       <Route path="/about" element={<About />} />
       <Route path="/services" element={<Services/>} />
@@ -22,3 +47,4 @@ function App() {
 }
 
 export default App;
+
