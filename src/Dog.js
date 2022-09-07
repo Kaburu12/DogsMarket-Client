9efbb81b4e0abbe402d71b852f71bbc87be5cef3
Michael Kaburu <104422948+Kaburu12@users.noreply.github.com>
2022-09-07 14:12:07 +0300

    Create Dog.js

---
+++ src/Dog.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const Dog = ({ dog, name, image, price ,onDeleteDog}) => {
+	
+	function handleDeleteClick() {
+		fetch(`https://server-app123.herokuapp.com/order/${dog.id}`, {
+		  method: "DELETE",
+		})
+		.then((r) => r.json())
+		.then(() => onDeleteDog(dog));
+	  
+	  }
+
+  return (
+	  <div id="dogcard">
+		   <div><button id="delete" onClick={handleDeleteClick}>❎</button></div> 
+			  <img src={ image} alt="" />
+			  <h4>{ name}</h4>
+			  <h5>{ price}</h5>
+		  </div>
+  );
+};
+
+export default Dog;
